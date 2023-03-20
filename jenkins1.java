cf90f5c749e04f5a2743933fe55434b26ba520f7
vishwsingh <118203821+vishwsingh@users.noreply.github.com>
2023-03-20 16:06:14 -0400

    Update jenkins1.java

--- jenkins1.java
+++ jenkins1.java
@@ -2,9 +2,8 @@ import java.util.*;
 
 public class Jenkins1 {
   public static void main(String[] args) {
+   
     
-//       Scanner input = new Scanner(System.in);
-//       String a = input.nextLine();
-      System.out.println(System.console.readLine());
+      System.out.println(System.console().readLine());
   }
 }
